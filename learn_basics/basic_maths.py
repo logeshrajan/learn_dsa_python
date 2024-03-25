2a2fb95ddd9ead32d9d8da935f61b70d284206cb
Logeshwaran S <42381154+logeshrajan@users.noreply.github.com>
2024-03-25 11:34:38 +0530

    Update basic_maths.py

--- learn_basics/basic_maths.py
+++ learn_basics/basic_maths.py
@@ -44,4 +44,34 @@ if reverse_number(n)==n:
 else:
     print(f"{n} is not Palindrome")
 
+# Amstrong
+
+def is_amstrong(n):
+    """
+    Armstrong Numbers are the numbers having the sum of digits raised to power no. of digits is equal to
+    a given number. Examples- 0, 1, 153, 370, 371, 407, and 1634 are some of the Armstrong Numbers.
+    """
+    old_n = n
+    l_n = len(str(n))
+    res = 0
+    while n>0:
+        res += (n%10)**l_n
+        n//=10
+    if old_n == res:
+        return f"yes {old_n} is an Amstrong number" 
+    else:
+        return f"yes {old_n} is Not an Amstrong number"
+    
+print(is_amstrong(153))
+
+# Print All Divisor
+def printDivisorsOptimal(n):
+    print("The Divisors of",n,"are:")
+    for i in range(1, int(n**0.5)+1):
+        if n % i == 0:
+            print(i, end=" ")
+            if i != n/i:
+                print(int(n/i), end=" ")
+    print()
+printDivisorsOptimal(36)
 

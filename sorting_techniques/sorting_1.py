a1ac76ce97b4dc842107af9702a9ed58788c86cd
Logeshwaran S <42381154+logeshrajan@users.noreply.github.com>
2024-03-28 10:40:14 +0530

    Update sorting_1.py

--- sorting_techniques/sorting_1.py
+++ sorting_techniques/sorting_1.py
@@ -1,5 +1,5 @@
 # Selection Sort Algorithm
-# get the minimum and swap it to left index. then increment the left index by 1.
+# get the minimum in range and swap it to left index. then increment the left index by 1.
 TC O(N^2) SC O(1)
 def selection_sort(arr):
     for l in range(len(arr)-1):
@@ -18,18 +18,18 @@ print('Before sorting',x)
 print('After sorting',selection_sort(x))
 
 # Bubble Sort Algorithm
-
+# take the highest element and place it in right most corner and repeat the same for each iteration
 def bubble_sort(arr):
     n = len(arr)
     # Traverse through all elements in the array
-    for i in range(n):
+    for l in range(n):
         swapped = False  # Initialize swapped flag
-        # Last i elements are already in place, so we don't need to check them again
-        for j in range(0, n-i-1):
-            # Traverse the array from 0 to n-i-1
+        # Last l elements are already in place, so we don't need to check them again
+        for r in range(0, n-l-1):
+            # Traverse the array from 0 to n-l-1
             # Swap if the element found is greater than the next element
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
+            if arr[r] > arr[r+1]:
+                arr[r], arr[r+1] = arr[r+1], arr[r]
                 swapped = True  # Set swapped flag to True if a swap is performed
         print(arr)
         # If no swap is performed in the inner loop, the array is already sorted
@@ -39,3 +39,22 @@ def bubble_sort(arr):
 x = [2,13, 4, 1, 3, 6, 28]
 print('Before sorting',x)
 print('After sorting',bubble_sort(x))
+
+# Insertion Sort Algorithm
+# Take an element and place it in correct position
+def insertion_sort(arr):
+    n = len(arr)
+    for l in range(n):
+        r = l
+        while r>0 and arr[r-1]>arr[r]:
+            arr[r-1], arr[r] = arr[r], arr[r-1] 
+            r-=1
+        print(arr)
+    return arr
+    
+x = [2,13, 4, 1, 3, 6, 28]
+print('Before sorting',x)
+print('After sorting',insertion_sort(x))
+
+
+
